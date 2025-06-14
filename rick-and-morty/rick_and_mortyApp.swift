629cf434c1e79966804a8080237a544c5ed9268f
Kuba <jakub.kucinski@icloud.com>
2025-06-14 22:44:12 +0200

    Initial Commit

---
+++ rick-and-morty/rick_and_mortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  rick_and_mortyApp.swift
+//  rick-and-morty
+//
+//  Created by Jakub Kuciński on 14/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct rick_and_mortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
